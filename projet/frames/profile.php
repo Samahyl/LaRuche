02dee11c0fafefee9b42a9a8b254e573635f0954
Samahyl <106036939+Samahyl@users.noreply.github.com>
2022-05-25 10:57:26 +0200

    Update profile.php

--- projet/frames/profile.php
+++ projet/frames/profile.php
@@ -17,18 +17,18 @@
 		?><div class="gestion_global"><?php
 		if ($_SESSION['habitilation'] == 3) {
 			?>
-				<a class='gestion' href='gestion_e'>Gestion des espaces</a>
-				<a class='gestion' href='gestion_r'>Gestion des réservation</a>
-				<a class='gestion' href='actus'>Gestion des Actualités</a>
+				<a class='gestion' href='gestion_e.php'>Gestion des espaces</a>
+				<a class='gestion' href='gestion_r.php'>Gestion des réservation</a>
+				<a class='gestion' href='actus.php'>Gestion des Actualités</a>
 			<?php
 		}
 		elseif ($_SESSION['habitilation'] == 2) {
 			?>
-				<a class='gestion' href='actus'>Gestion des Actualités</a>
+				<a class='gestion' href='actus.php'>Gestion des Actualités</a>
 			<?php
 		}
 		?>
-			<a class='gestion' href='self_reservation'>Mes réservations</a>
+			<a class='gestion' href='self_reservation.php'>Mes réservations</a>
 		</div><?php
 	?>
 	<div class="profile_info">
@@ -37,10 +37,10 @@
 			<li>Nom : <?php echo $_SESSION['nom']?></li>
 			<li>Prénom : <?php echo $_SESSION['prenom']?></li>
 			<li>Rejoins le : <?php echo $_SESSION['date_reservation']?></li>
-			<li><a href="../assets/libraries/update_profile">Mettre à jour les informations</a></li>
+			<li><a href="../assets/libraries/update_profile.php">Mettre à jour les informations</a></li>
 		</ul>
 	</div>
-	<p id="signin"><a href="login?sw=relog">Se déconnecter</a></p>
+	<p id="signin"><a href="login.php?sw=relog">Se déconnecter</a></p>
 </body>
 </html>
 <?php
