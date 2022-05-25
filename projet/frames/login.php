c0ba6b49d2719d5aa5953ef61d8456606bac2e94
Samahyl <106036939+Samahyl@users.noreply.github.com>
2022-05-25 10:50:14 +0200

    Update login.php

--- projet/frames/login.php
+++ projet/frames/login.php
@@ -3,10 +3,10 @@
 	include "../assets/libraries/header_nav.php"; //Intégration de mise en page + header HTML
 	include "../assets/libraries/connexion_DB.php"; //Intégration de connexion à la base de données
 	if (isset($_GET['sw']) == "relog") { //Si première connexion ou changement de compte
-		$lnk = "../../assets/libraries/action?sw=relog"; //Variable de lien en fonction de la condition
+		$lnk = "../../assets/libraries/action.php?sw=relog"; //Variable de lien en fonction de la condition
 	}
 	else { //Sinon
-		$lnk = "../../assets/libraries/action"; //Variable de lien en fonction de la condition
+		$lnk = "../../assets/libraries/action.php"; //Variable de lien en fonction de la condition
 	} //Fin si
 ?>
 
@@ -22,6 +22,6 @@
 		<input type="password" id ="password" name="password" placeholder="Mot de Passe" value=""> <!-- Champs de mot de passe  -->
 		<input id="submitbutton" type="submit" name="submit" value="Ok"> <!-- Envoie des données pour connexion -->
 	</form> <!-- Fin formulaire -->
-	<p id="signin">Ou <a href="../../frames/signin"><strong>s'inscrire</strong></a></p> <!-- Rédirection page d'inscription -->
+	<p id="signin">Ou <a href="../../frames/signin.php"><strong>s'inscrire</strong></a></p> <!-- Rédirection page d'inscription -->
 </body> <!-- Fin corps -->
 </html> <!-- Fin code HTML -->
